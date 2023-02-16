80f5c58772aab49e19a4da648fc921071039010e
Daniel <90052588+daniSoares1993@users.noreply.github.com>
2023-02-16 16:21:09 -0300

    Fix: test render

--- leitor_epub/src/app/tab1/tab1.page.ts
+++ leitor_epub/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var require: any
 
 @Component({
   selector: 'app-tab1',
@@ -9,4 +10,11 @@ export class Tab1Page {
 
   constructor() {}
 
+  lerLivro(){
+    const ePub = require('../../service/service.js');
+    let book = ePub('../../assets/livros/Menino_de_Ouro_Claire_Adam.epub');
+    let rendition = book.renderTo("area");
+    rendition.display();
+    console.log(book);
+  }
 }
