a6ab942970769e34b2649080c9be738ba4e77e7f
Daniel <90052588+daniSoares1993@users.noreply.github.com>
2023-02-24 17:28:00 -0300

    Fix: render book

--- leitor_epub/src/app/tab1/tab1.page.ts
+++ leitor_epub/src/app/tab1/tab1.page.ts
@@ -7,17 +7,21 @@ declare var require: any
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  public book: any = '';
-  public rendition:any = '';
+  // public book: any = '';
+  // public rendition:any = '';
 
   constructor() {
+    const ePub = require('../../service/service.js');
+    var book = ePub('../../assets/livros/Menino_de_Ouro_Claire_Adam.epub');
+    var rendition = book.renderTo("area", {width: 600, height: 400,allowScriptedContent: true});
+    rendition.display();
   }
   
-  lerLivro(){
-    const ePub = require('../../service/service.js');
-    this.book = ePub('../../assets/livros/Menino_de_Ouro_Claire_Adam.epub');
-    this.rendition = this.book.renderTo("area", { method: "default", width: "100%", height: "100%" });
-    console.log(this.book);
-    console.log(this.rendition);
-  }
+  // lerLivro(){
+  //   const ePub = require('../../service/service.js');
+  //   this.book = ePub('../../assets/livros/Menino_de_Ouro_Claire_Adam.epub');
+  //   this.rendition = this.book.renderTo("area", { method: "default", width: "100%", height: "100%" });
+  //   console.log(this.book);
+  //   console.log(this.rendition);
+  // }
 }
