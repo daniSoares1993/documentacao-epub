4d041b7218d16821dcdb05742bec4860e947dd46
Daniel <90052588+daniSoares1993@users.noreply.github.com>
2023-03-03 15:37:24 -0300

    Fix: update

--- leitor_epub/src/app/tab1/tab1.page.ts
+++ leitor_epub/src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
 declare var require: any
 
 @Component({
@@ -10,18 +10,30 @@ export class Tab1Page {
   public ePub:any = require('../../service/service.js');
   public book:any = this.ePub('../../assets/livros/Menino_de_Ouro_Claire_Adam.epub');
   public rendition:any = this.book.renderTo("area", {flow: "paginated", width: 365, height: 560,allowScriptedContent: true});
-  
+  public paginaAtual:any = '';
   constructor() {
   }
+
+  ngOnInit() {
+    this.paginaAtual = localStorage.getItem('paginaAtual')
+  }
+
   showBook(){
-    this.rendition.display();
+    if(this.paginaAtual === 'undefined' || this.paginaAtual === null ){
+      this.rendition.display();
+    }else{
+      this.paginaAtual = JSON.parse(this.paginaAtual);
+      this.rendition.display(this.paginaAtual.end.cfi);
+    }
   }
   previous(){
-   this.rendition.prev();
+    this.rendition.prev();
+    localStorage.setItem('paginaAtual', JSON.stringify(this.rendition.location));
   }
 
   foward(){
-   this.rendition.next();
+    this.rendition.next();
+    localStorage.setItem('paginaAtual', JSON.stringify(this.rendition.location));
   }
   
 }
