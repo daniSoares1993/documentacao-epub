61c68ed70325234bfb1d11b29957cb4f39f355c4
Daniel <90052588+daniSoares1993@users.noreply.github.com>
2023-03-09 14:46:26 -0300

    Fix: vai exibir a display igual a range.startcontainer

--- leitor_epub/src/app/tab1/tab1.page.ts
+++ leitor_epub/src/app/tab1/tab1.page.ts
@@ -8,9 +8,13 @@ declare var require: any
 })
 export class Tab1Page {
   public ePub:any = require('../../service/service.js');
+  public Annotations:any = this.ePub.Annotations;
   public book:any = this.ePub('../../assets/livros/Menino_de_Ouro_Claire_Adam.epub');
   public rendition:any = this.book.renderTo("area", {flow: "paginated", width: 365, height: 560,allowScriptedContent: true});
   public paginaAtual:any = '';
+  public text:any = '';
+  public selection:any;
+  public areaConteudo:any = document.getElementById('area');
   constructor() {
   }
 
@@ -25,15 +29,27 @@ export class Tab1Page {
       this.paginaAtual = JSON.parse(this.paginaAtual);
       this.rendition.display(this.paginaAtual.end.cfi);
     }
+    this.book.getRange(this.paginaAtual.end.cfi).then(function(range:any){
+      console.log("Faixa de conteúdo da página 3:", range.startContainer);
+    });
+    this.rendition.annotations.underline();
   }
   previous(){
     this.rendition.prev();
+    console.log(this.rendition.reportLocation());
+    console.log(this.rendition.currentLocation())
     localStorage.setItem('paginaAtual', JSON.stringify(this.rendition.location));
   }
 
   foward(){
     this.rendition.next();
+    console.log(this.rendition.reportLocation());
+    console.log(this.rendition.currentLocation());
     localStorage.setItem('paginaAtual', JSON.stringify(this.rendition.location));
+  }
+
+  teste(){
+    console.log('clicou');
   }
   
 }
